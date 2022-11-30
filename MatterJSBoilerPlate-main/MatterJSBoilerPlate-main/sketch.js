6683dfcf3825bdc0e6ca20d0ee39409184de08b3
JamesZboi <114061950+JamesZboi@users.noreply.github.com>
2022-11-30 19:25:48 +1100

    Add files via upload

---
+++ MatterJSBoilerPlate-main/MatterJSBoilerPlate-main/sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+	//Create the Bodies Here.
+
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+	}
+
+	Engine.run(engine);
+  
+ groundObj.display();
+
+ groundObj=new ground(width/2,670,width,20);
+ leftSide = new ground(1100,600,20,120);
+
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+	Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+ }
+}
